79b143e203f79cfe1f7458ebd6b489dc1b8f016e
Arman-Kabir <armankabir8@gmail.com>
2024-01-04 10:10:51 +0600

    password hashed using bcryptjs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,19 @@
 import mongoose from 'mongoose';
 import User from '../models/User.js';
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
     try {
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(req.body.password, salt);
         console.log(req.body);
-        // const newUser = new User(req.body);
+        const newUser = new User({ ...req.body, password:hash});
+        console.log(newUser);
+
+        await newUser.save();
+        res.status(200).send("User has been created");
     } catch (err) {
         // todo
+        
     }
 }
