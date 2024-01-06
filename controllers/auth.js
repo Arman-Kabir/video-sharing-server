6f74696aca402d1b6e81581ef578304c3a922c7c
Arman-Kabir <armankabir8@gmail.com>
2024-01-06 12:04:46 +0600

    sign in done

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,23 +22,21 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => {
     try {
-        const user = await User.findOne({name:req.body.name});
-        if(!user) return next(createError(404,"user not found"));
+        const user = await User.findOne({ name: req.body.name });
+        if (!user) return next(createError(404, "user not found"));
 
-        const isCorrect = await bcrypt.compare(req.body.password,user.password);
+        const isCorrect = await bcrypt.compare(req.body.password, user.password);
 
-        if(!isCorrect) return next(createError(400,"Wrong credentials"));
+        if (!isCorrect) return next(createError(400, "Wrong credentials"));
 
-        const token = jwt.sign({id:user._id},process.env.JWT);
+        const token = jwt.sign({ id: user._id }, process.env.JWT);
 
-        const {} = user;
+        const { password, ...others } = user._doc;
 
-        res.cookie("access_token",token,{
-            httpOnly:true
-        }).status(200).json(user)
-
-
-
+        res.cookie("access_token", token, {
+            httpOnly: true
+        }).status(200).json(others)
+        
     } catch (err) {
         // todo
         next(err);
